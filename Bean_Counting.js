f449a125134bea7db6b28dd36ba15e457336d489
vamsikrishnanunna-1998 <nunnavamsikrishna1998@msitprogram.net>
2020-04-28 11:22:31 +0530

    added countchar with uppercase

--- Bean_Counting.js
+++ Bean_Counting.js
@@ -8,4 +8,15 @@ function countBs(String) {
   return count;
 }
 
+function countChar(String,char) {
+  var count = 0;
+  for (var str of String) {
+    if (str == char.toUpperCase()) {
+      count++;
+    }
+  }
+  return count;
+}
+
 console.log(countBs("Bnab"));
+console.log(countChar("baNaNn","N"));
